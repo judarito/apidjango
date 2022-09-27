bd4824dc9b0dc02a15352009f9168f446cdb852d
Juan David Ricardo <juan.d.ricardo.t@gmail.com>
2022-09-27 16:25:10 -0500

    Mi primer commit

---
+++ productos/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .api import ProductoViewSet
+
+router=routers.DefaultRouter()
+
+router.register('api/productos', ProductoViewSet,'productos')
+
+urlpatterns = router.urls
